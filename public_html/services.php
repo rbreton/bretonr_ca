20da103f6da8606b720a24711334f050381213de
Remz <bretonr@gmail.com>
2022-04-17 15:28:47 -0400

    V01 - Main/Origin

--- public_html/services.php
+++ public_html/services.php
@@ -17,14 +17,29 @@
 		<?php include ("{$root}tpl/header.php"); ?>
 		<div class="centerContent content">
 			<h1 class="center">Offre de services</h1>
-			<h2>Services offerts</h2>
-			<ul class="dashed">
-				<li>Correction et mise à jours</li>
-				<li>Migration vers un nouvel hébergeur</li>
-				<li>Changement de votre nom de domaine</li>
-				<li>Création, modification de votre logo et de votre visuel</li>
-				<li>Correction des "Bugs" tant d'affichage que de programmation</li>
-			</ul>
+			<div class="oneCol">
+				<h2>Services offerts</h2>
+				<ul class="dashed">
+					<li>Correction et mise à jours</li>
+					<li>Migration vers un nouvel hébergeur</li>
+					<li>Changement de votre nom de domaine</li>
+					<li>Correction des "Bugs" tant d'affichage que de programmation</li>
+					<li>Mise à jours de votre plateforme wordpress ou autre</li>
+				</ul>
+			</div>
+			<div class="endSection"><p>-------</p></div>
+			<div class="oneCol">
+				<h3>Correction</h3>
+				<p>Pour toutes corrections ou bien mise à jours de votre contenu.  Ajout de texte, ajout d'image, ajout de plugin ou bien tout autres corrections.</p>
+				<h3>Migration</h3>
+				<p>Vous changer de services d'hébergement, mais vous n'avez aucune idée comment faire pour migrer votre site vers le nouveau serveur.  Ce services vous offre la migration complete, votre nom domaine - s'il est hébergé au même endroit, vos données - tant vos fichiers que votre base de données, vos adresses courriels et tout autres informations à propos de votre site web.</p>
+				<h3>Changement de nom de domaine</h3>
+				<p>Vous changer de nom de compagnie et vous voulez changer votre nom de domaine?  Ce service vous permet de changer votre nom de domaine sans avoir d'intéruption.</p>
+				<h3>Correction des "Bugs"</h3>
+				<p>Vous avez mis à jours votre site internet et votre site ne fonctionne plus, ou bien des "bugs" on apparu avec le temps, ou bien tout autres problèmes avec votre site internet.</p>
+				<h3>Mise à jours</h3>
+				<p>Pour mettre tout vos plugins ou bien votre module wordpress à jours.  Ce service vous offre la tranquilité d'esprit que votre site sera à jours et qu'un "back-up" complet sera fait pour évité la perte de vos données.</p>
+			</div>
 		</div>
 		<?php include ("{$root}tpl/footer.php"); ?>
 	</body>
