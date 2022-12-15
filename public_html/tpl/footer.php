514be94279d635275bab307e7ef9b14d160049d4
Remz <bretonr@gmail.com>
2022-12-14 22:43:48 -0500

    [ADD] Goggle analytic + sitemap file

--- public_html/tpl/footer.php
+++ public_html/tpl/footer.php
@@ -4,3 +4,13 @@
 				<p class="txtCenter smallTxt">Feeling nostalgics come over <a href="http://old.bretonr.ca/">here</a></p>
 			</div>
 		</footer>
+		
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-DHBCHB8Z49"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-DHBCHB8Z49');
+</script>
