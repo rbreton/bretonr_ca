4832d9fce31a1988996410553506d138b0225a5b
Remz <bretonr@gmail.com>
2023-03-18 20:26:57 -0400

    [ADD] Mobile navigation

--- public_html/tpl/header.php
+++ public_html/tpl/header.php
@@ -1,18 +1,20 @@
 		<header id='stickyNav'>
-			<div class='centerContent'>
-				<div class='left logo'>
+			<div class='centerContent mainNavContener'>
+				<div class='logo left'>
 					<div class='lWrapper'><div class='lDarkWhite'></div><div class='lDarkBlue'></div></div><div class='lLightBlue left'></div><div class='lSignature left'><a href="/"><span class='lTinyLetterSpacing'>breton</span><span class='lItalic'>r</span></a><blink class='txtCursor'>_</blink></div>
 				</div>
+				<input class="statusChecker" type="checkbox" />
+				<div class="hamLine">
+          <span class="line line1"></span>
+          <span class="line line2"></span>
+          <span class="line line3"></span>
+        </div>
 				<nav class="mainNav right">
-					<ul>
+					<ul class="dashed menuItem">
 						<li><a href="<?php echo $root; ?>">Accueil</a></li>
-						<li>-</li>
 						<li><a href="<?php echo $root; ?>services.php">Services</a></li>
-						<li>-</li>
 						<li><a href="<?php echo $root; ?>prix.php">Prix</a></li>
-						<li>-</li>
 						<li><a href="<?php echo $root; ?>aPropos.php">À propos</a></li>
-						<li>-</li>
 						<li><a href="<?php echo $root; ?>contact.php">Contact</a></li>
 						<div class="clear"></div>
 					</ul>
