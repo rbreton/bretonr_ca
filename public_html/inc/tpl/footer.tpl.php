5fcee8169e2f24918443efcf9070e56e7b6d8e34
Remz <bretonr@gmail.com>
2025-05-08 10:50:01 -0400

    Correction Texte footer v3.0.2

--- public_html/inc/tpl/footer.tpl.php
+++ public_html/inc/tpl/footer.tpl.php
@@ -1,7 +1,7 @@
 		<footer>
 			<div class="centerContent">
 				<p class="txtCenter notPadded">Copyright © <?php echo date("Y"); ?> Rémi Breton - Tous droits réservés.</p>
-				<p class="txtCenter smallTxt notPadded">Feeling nostalgics come over <a target="_blank" href="https://old.bretonr.ca/public/index.php">here</a></p>
+				<p class="txtCenter smallTxt notPadded">Retour à la Version <a target="_blank" href="https://old.bretonr.ca/public/index.php">1.0</a></p>
 			</div>
 		</footer>
 		
